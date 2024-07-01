3674ab9fb0a11780857105151f0760dc34d7a7b3
HouraHashemi <h99.hashemi@gmial.com>
2024-07-01 22:30:07 +0330

    user can access to its profile by customer viewset and serializer.

--- Users/serializers.py
+++ Users/serializers.py
@@ -5,20 +5,19 @@ from .models import Customer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
-    
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
 
 class UserSerializer(BaseUserSerializer):
-    
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField()
+    user_id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Customer
         fields = ['id', 'user_id', 'bio']
+
--- Users/views.py
+++ Users/views.py
@@ -4,7 +4,23 @@ from rest_framework.viewsets import GenericViewSet
 from .models import Customer
 from .serializers import CustomerSerializer
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 # Create your views here.
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    
+
+    @action(detail=False, methods=['GET', 'PUT'])
+    def me(self, request):
+        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
