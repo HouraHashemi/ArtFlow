098c12066679467f6fe5cc2368526c759b73f8c7
HouraHashemi <h99.hashemi@gmial.com>
2024-07-04 14:22:19 +0330

    postgresql connected by DBeaver

--- config/settings.py
+++ config/settings.py
@@ -107,12 +107,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rra_db',   # Replace with your database name
+        'USER': 'rra_user',   # Replace with your database username
+        'PASSWORD': '1234',   # Replace with your database password
+        'HOST': 'localhost',    # Replace with your database host, if necessary
+        'PORT': '5432',             # Replace with your database port, if necessary
     }
 }
+
 
 
 # Password validation
@@ -186,4 +198,3 @@ DJOSER = {
         'current_user': 'Users.serializers.UserSerializer'
     }
 }
-
